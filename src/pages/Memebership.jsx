f16c49af7b762e1c21456eb3b408499896b93e18
MugoBrian <mikanmuchiri@gmail.com>
2025-09-26 19:06:18 +0300

    Added the membership cards and payment section.

--- src/pages/Memebership.jsx
+++ src/pages/Memebership.jsx
@@ -1,6 +1,12 @@
 import { useEffect } from "react";
 import MembershipHero from "../assets/images/MembershipHero.png";
 import { useHeader } from "../context/useHeaderContext";
+import { MembershipCard, MemebershipSectionHeader } from "../components";
+import HomeHero from "../assets/images/HomeHero.png";
+
+import membershipData from "../lib/data/memberships.json";
+import { PaymentSection } from "../components/membership";
+
 function Memebership() {
   const { setTitle, setDescription, setImage } = useHeader();
   const title = "Membership";
@@ -12,7 +18,35 @@ function Memebership() {
     setDescription(description);
     setImage(MembershipHero);
   }, [setImage, setTitle, setDescription]);
-  return <div> Memebership</div>;
+  return (
+    <div className="flex-col gap-6 bg-white-50 pt-14 pb-12 text-center px-4 lg:px-28 sm:px-3 md:px-6 xl:px-32 2xl:px-30">
+      <div className="flex flex-col gap-10">
+        <div className="flex flex-col gap-8">
+          <MemebershipSectionHeader heading="INDIVIDUAL" />
+          <div className="grid grid-cols-1 md:grid md:grid-cols-2 gap-8">
+            {membershipData
+              .filter((membership) => membership.type == "individual")
+              .map((membership, index) => (
+                <MembershipCard key={index} {...membership} isPage="true" />
+              ))}
+          </div>
+        </div>
+        <div className="flex flex-col gap-8">
+          <MemebershipSectionHeader heading="COUPLES" />
+          <div className="grid grid-cols-1 md:grid md:grid-cols-2 gap-8">
+            {membershipData
+              .filter((membership) => membership.type == "couple")
+              .map((membership, index) => (
+                <MembershipCard key={index} {...membership} isPage="true" />
+              ))}
+          </div>
+        </div>
+      </div>
+      <div className="bg-white-50 mt-16">
+        <PaymentSection />
+      </div>
+    </div>
+  );
 }
 
 export default Memebership;
