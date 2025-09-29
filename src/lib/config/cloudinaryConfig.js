54733ef92eb3e0418c4cd0f733049d0f7318316a
MugoBrian <mikanmuchiri@gmail.com>
2025-09-29 11:46:38 +0300

    Removed unused import statement.

---
+++ src/lib/config/cloudinaryConfig.js
@@ -0,0 +1,12 @@
+// import { v2 as cloudinary } from "cloudinary";
+
+// const cloudinaryConfig = cloudinary.config({
+//   cloud_name: import.meta.env.CLOUDINARY_CLOUD_NAME,
+//   api_key: import.meta.env.CLOUDINARY_API_KEY,
+//   api_secret: import.meta.env.CLOUDINARY_API_SECRET,
+// });
+
+export const CLOUD_NAME = import.meta.env.VITE_CLOUDINARY_CLOUD_NAME;
+export const TAG = "giantgolem-images";
+
+// export default cloudinaryConfig;
