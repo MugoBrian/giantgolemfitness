8880928ffac52547ca3961b14db5aea7fea2ddfb
MugoBrian <mikanmuchiri@gmail.com>
2025-09-29 11:49:52 +0300

    Modularization of pages.

---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+import Contact from "./Contact";
+import Gallery from "./Gallery";
+import Home from "./Home";
+import NotFoundPage from "./NotFoundPage";
+import Programs from "./Programs";
+
+export { Contact, Gallery, Home, NotFoundPage, Programs };
